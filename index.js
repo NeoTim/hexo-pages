f8b8227ba1eef35f4316209f8ae6bca971d95fdb
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-02-22 13:09:15 +0800

    fix generate bug

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 'use strict';
 
-var rimraf = require('rimraf');
-var fs = require('fs-extra');
 var path = require('path');
+var fs = require('fs');
+var ncp = require('ncp');
+var rimraf = require('rimraf');
 
-hexo.on('generateBefore', function () {
+hexo.on('generateAfter', function () {
   var source = path.join(process.cwd(), 'pages');
   var destination = path.join(process.cwd(), 'public');
 
-  try {
-    rimraf.sync(destination);
-    fs.copySync(source, destination);
-    console.log('Pages generated');
-  } catch (err) {
-    throw new Error(err);
-  }
+  fs.readdir(source, function (err, files) {
+    var fullPaths = files.map(function (item) {
+      return path.join(destination, item);
+    });
+
+    fullPaths.forEach(function (item) {
+      rimraf.sync(item);
+    });
+
+    ncp(source, destination, function (err) {
+      if (err) {
+        throw new Error(err);
+      }
+
+      console.log('Pages generated');
+    });
+  });
 
 });
