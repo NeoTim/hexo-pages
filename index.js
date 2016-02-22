4717dc43ec768ef905159862bc93d5dfa2cd6fae
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-02-22 11:56:22 +0800

    add   console.log(-------------);

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 'use strict';
 
 var ncp = require('ncp').ncp;
+var rimraf = require('rimraf');
 var path = require('path');
 
 hexo.on('generateBefore', function () {
   var source = path.join(process.cwd(), 'pages');
   var destination = path.join(process.cwd(), 'public');
 
-  ncp(source, destination, function (err) {
+  rimraf(destination, function (err) {
     if (err) {
       throw new Error(err);
     }
+
+    ncp(source, destination, function (err) {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+
+    console.log('Pages generated');
   });
 
-  console.log('Pages generated');
 });
