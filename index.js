70698ccc16b2a563760f068f80bd552858b82242
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-03-02 20:24:06 +0800

    refactor

--- index.js
+++ index.js
@@ -1,48 +1,12 @@
-'use strict';
+var merge = require('utils-merge');
 
-var path = require('path');
-var fs = require('fs');
-var ncp = require('ncp');
-var rimraf = require('rimraf');
+var config = hexo.config.pages = merge({
+  dir: 'pages'
+}, hexo.config.pages);
 
-// hexo.on('deployBefore', function () {
-hexo.on('generateAfter', function() {
-  var source = path.join(process.cwd(), 'pages');
-  var destination = path.join(process.cwd(), 'public');
-
-  if(!fsExistsSync(source)) {
-    throw new Error('pages dir not found');
-  }
-
-  if(!fsExistsSync(destination)) {
-    fs.mkdirSync(destination);
-  }
-
-  fs.readdir(source, function(err, files) {
-    var fullPaths = files.map(function(item) {
-      return path.join(destination, item);
-    });
-
-    fullPaths.forEach(function(item) {
-      rimraf.sync(item);
-    });
-
-    ncp(source, destination, function(err) {
-      if (err) {
-        throw new Error(err);
-      }
-
-      console.log('Pages generated');
-    });
-  });
-
-});
-
-function fsExistsSync(path) {
-  try {
-    fs.accessSync(path, fs.F_OK);
-  } catch (e) {
-    return false;
-  }
-  return true;
+// Set default search path
+if (!config.path) {
+  config.path = 'pages';
 }
+
+hexo.extend.generator.register('pages', require('./lib/pages'));
